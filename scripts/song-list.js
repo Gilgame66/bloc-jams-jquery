6f4706587b9d20dd82f4fbee67cb6dd2c5b08c99
Harrison <harrisonklostreich@gmail.com>
2020-08-24 10:00:58 -0400

    Make 'previous button' changes

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,6 +16,7 @@
 
     song.element.on('click', event => {
       player.playPause(song);
+      $('button#playy-pause').attr('playState', player.playState);
     });
 
     $('#song-list').append(song.element);
